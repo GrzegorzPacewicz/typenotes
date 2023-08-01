1f0bad07d7d9b1225e4538e7299bc5e894284934
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-01 13:20:54 +0200

    Setting deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,11 @@ function App() {
     return (
         <HashRouter>
             <Layout>
-
                 <Routes>
                     <Route exact path="/*" element={<Notes/>}/>
                     <Route path="/create/" element={<CreateNote/>}/>
                     <Route path="/edit/:id" element={<EditNote />} />
                 </Routes>
-
             </Layout>
         </HashRouter>
     )
