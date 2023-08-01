091a1de85e5debd4ef4ec202af89c67bd4e92c66
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-01 21:29:00 +0200

    App moved to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import CreateNote from "./pages/CreateNote/CreateNote";
-import Notes from "./pages/Notes/Notes";
-import Layout from "./components/Layout/Layout";
-import EditNote from "./components/EditNote/EditNote";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import CreateNote from './pages/CreateNote/CreateNote';
+import Notes from './pages/Notes/Notes';
+import Layout from './components/Layout/Layout';
+import EditNote from './components/EditNote/EditNote';
 
-function App() {
+const App: React.FC = () => {
     return (
         <HashRouter>
             <Layout>
                 <Routes>
-                    <Route exact path="/*" element={<Notes/>}/>
-                    <Route path="/create/" element={<CreateNote/>}/>
+                    <Route path="/" element={<Notes />} />
+                    <Route path="/create" element={<CreateNote />} />
                     <Route path="/edit/:id" element={<EditNote />} />
+                    <Route path="*" element={<Notes />} />
                 </Routes>
             </Layout>
         </HashRouter>
-    )
-}
+    );
+};
 
-export default App
+export default App;
