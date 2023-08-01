d962fdae4d930108091837818b96ae648bc32cce
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-01 21:24:06 +0200

    Notes moved to TS

--- src/pages/Notes/Notes.tsx
+++ src/pages/Notes/Notes.tsx
@@ -1,31 +1,39 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 import NoteCard from '../../components/NoteCard/NoteCard';
 import Masonry from '@mui/lab/Masonry';
 
-const Notes = () => {
-    const [notes, setNotes] = useState([]);
+interface Note {
+    id: string;
+    title: string;
+    category: string;
+    details: string;
+}
+
+const Notes: React.FC = () => {
+    const [notes, setNotes] = useState<Note[]>([]);
 
     useEffect(() => {
-        const storedNotes = JSON.parse(localStorage.getItem('notes')) || [];
+        const storedNotes: Note[] = JSON.parse(localStorage.getItem('notes') || '[]');
         setNotes(storedNotes);
     }, []);
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-    const handleDelete = (id) => {
+
+    const handleDelete = (id: string) => {
         const newNotes = notes.filter((note) => note.id !== id);
         setNotes(newNotes);
         localStorage.setItem('notes', JSON.stringify(newNotes));
     };
 
     return (
-        <Container sx={{marginTop: '20px'}}>
-            <Masonry spacing={3} columns={{xs: 1, md: 2, lg: 3}}>
+        <Container sx={{ marginTop: '20px' }}>
+            <Masonry spacing={3} columns={{ xs: 1, md: 2, lg: 3 }}>
                 {notes.map((note) => (
                     <div key={note.id}>
-                        <NoteCard note={note} handleDelete={handleDelete}/>
+                        <NoteCard note={note} handleDelete={handleDelete} />
                     </div>
                 ))}
             </Masonry>
