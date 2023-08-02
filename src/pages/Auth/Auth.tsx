84d80137cb4ed084347125d04f722e167431f6e4
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-02 23:08:41 +0200

    Basic auth method added. With email and password

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import { Box, Button, Input } from "@mui/material";
+import React, {useState} from 'react';
+import {Box, Button, Input} from "@mui/material";
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import {auth} from "../../config/firebase"
 
 const Auth: React.FC = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const signIn = async () => {
+        await createUserWithEmailAndPassword(auth, email, password);
+    };
+
     return (
-        <Box display="flex"  gap={2}>
-            <Input placeholder="Email" />
-            <Input placeholder="Password" />
-            <Button>Sign In</Button>
+        <Box display="flex" gap={2} justifyContent="center">
+            <Input
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+            />
+            <Input
+                placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <Button onClick={signIn}>Sign In</Button>
         </Box>
     );
 };
