711898fb62cc6ac9700dae55b68d67c45d4be05f
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-02 20:56:11 +0200

    ReactDOM.createRoot added to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import './index.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <App />
         </ThemeProvider>
     </React.StrictMode>,
-    document.getElementById('root')
 );
