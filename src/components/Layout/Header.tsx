50aac543dc2b55840aefc7cdb9d56e3ab796fa7d
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-02 17:28:02 +0200

    Changed Header.tsx style

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,65 +1,59 @@
 import React from 'react';
-import {Box, IconButton, Menu, MenuItem, Tooltip} from "@mui/material";
+import {Box, IconButton, Menu, MenuItem, Tooltip, Typography} from "@mui/material";
 import {StyledDate} from "./styled";
 import {format} from "date-fns";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import {AccountCircle} from "@mui/icons-material";
 
 const Header = () => {
 
-    const [auth, setAuth] = React.useState(true);
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAuth(event.target.checked);
-    };
-    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
     };
 
     return (
+
         <Box sx={{width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
             <StyledDate variant="body2" sx={{mr: 2}}>
                 Today is the {format(new Date(), `do MMMM Y`)}
             </StyledDate>
-            {auth && (
-                <div>
-                    <Tooltip title="Open settings">
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleMenu}
-                            color="inherit"
-                        >
-                            <AccountCircle/>
-                        </IconButton>
-                    </Tooltip>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        sx={{mt: '45px'}}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose}>Login</MenuItem>
-                        <MenuItem onClick={handleClose}>Logut</MenuItem>
-                    </Menu>
-                </div>
-            )}
+
+            <Box sx={{flexGrow: 0}}>
+                <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}
+                                color="inherit">
+                        <AccountCircle/>
+                    </IconButton>
+                </Tooltip>
+                <Menu
+                    sx={{mt: '45px'}}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                >
+
+                    <MenuItem onClick={handleCloseUserMenu}> <Typography>Login</Typography></MenuItem>
+                    <MenuItem onClick={handleCloseUserMenu}> <Typography>Logut</Typography></MenuItem>
+
+                </Menu>
+            </Box>
+
         </Box>
+
     );
 };
 
