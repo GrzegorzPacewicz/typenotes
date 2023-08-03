7b8e18f966e74c6a647c960684010236266b1e58
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-03 08:24:22 +0200

    Login with Google added. Logout added

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,31 +1,63 @@
 import React, {useState} from 'react';
-import {Box, Button, Input} from "@mui/material";
-import {createUserWithEmailAndPassword} from "firebase/auth"
-import {auth} from "../../config/firebase"
+import {Box, Button, Container, Input} from "@mui/material";
+import {createUserWithEmailAndPassword, signInWithPopup, signOut} from "firebase/auth"
+import {auth, GoogleProvider} from "../../config/firebase"
 
 const Auth: React.FC = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    console.log(auth?.currentUser?.email);
+
     const signIn = async () => {
-        await createUserWithEmailAndPassword(auth, email, password);
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.error(err)
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, GoogleProvider);
+        } catch (err) {
+            console.error(err)
+        }
+    };
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.error(err)
+        }
     };
 
     return (
-        <Box display="flex" gap={2} justifyContent="center">
-            <Input
-                placeholder="Email"
-                onChange={(event) => setEmail(event.target.value)}
-            />
-            <Input
-                placeholder="Password"
-                type="password"
-                onChange={(event) => setPassword(event.target.value)}
-            />
-            <Button onClick={signIn}>Sign In</Button>
-        </Box>
-    );
+        <Container>
+            <Box display="flex" gap={2} justifyContent="center">
+                <Input
+                    placeholder="Email..."
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                <Input
+                    placeholder="Password..."
+                    type="password"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <Button onClick={signIn}>Sign In</Button>
+
+            </Box>
+            <Box display="flex" gap={2} justifyContent="center" mt={10}>
+                <Button onClick={signInWithGoogle}>Sign In With Google</Button>
+            </Box>
+            <Box display="flex" gap={2} justifyContent="center" mt={10}>
+                <Button onClick={logout}>Logout</Button>
+            </Box>
+        </Container>
+
+    )
+        ;
 };
 
 export default Auth;
