826c08c57fb9768bff1c60b427f895ead93d63ad
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-09 06:05:23 +0200

    Changed geNoteList function for better type in TS.

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { collection, deleteDoc, doc, getDocs, QuerySnapshot } from "firebase/firestore";
-import { db, auth, storage } from "../../config/firebase";
-import { ref, uploadBytes } from "firebase/storage"
+import { auth, db } from "../../config/firebase";
 import NoteCard from "../../components/NoteCard";
 import Masonry from "@mui/lab/Masonry";
 import { Note } from '../../types';
-import { Button, Container, Input } from "@mui/material";
+import { Container } from "@mui/material";
 
 const Notes: React.FC = () => {
     const [notesList, setNotesList] = useState<Note[]>([]);
     const notesCollectionRef = collection(db, "Notes");
-    const [fileUpload, setFileUpload] = useState(null)
 
     const getNotesList = async () => {
         try {
             const data: QuerySnapshot = await getDocs(notesCollectionRef);
-            const filteredData: Note[] = data.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data() as Note,
-                userId: auth?.currentUser?.uid,
-            }));
+            const filteredData: Note[] = data.docs.map((doc) => {
+                const { id, ...noteData } = doc.data() as Note;
+                return {
+                    id: doc.id,
+                    ...noteData,
+                    userId: auth?.currentUser?.uid,
+                };
+            });
             setNotesList(filteredData);
         } catch (err) {
             console.error(err);
@@ -40,17 +41,6 @@ const Notes: React.FC = () => {
         window.scrollTo(0, 0);
     }, []);
 
-const uploadFile = async () => {
-    if (!fileUpload) return;
-    const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
-    try {
-        await uploadBytes(filesFolderRef, fileUpload);
-    }
-   catch(err) {
-        console.error(err);
-   }
-}
-
     return (
         <Container sx={{marginTop: '20px'}}>
             <Masonry spacing={3} columns={{xs: 1, md: 2, lg: 3}}>
@@ -60,8 +50,6 @@ const uploadFile = async () => {
                     </div>
                 ))}
             </Masonry>
-            <Input type="file" onChange={(event) => setFileUpload(event.target.files[0])}/>
-            <Button onClick={uploadFile}>Submit</Button>
         </Container>
     );
 };
--- src/types/Note.ts
+++ src/types/Note.ts
@@ -1,7 +1,7 @@
 export type CategoryType = "money" | "todos" | "reminders" | "work";
 
 export interface Note {
-    id?: string;
+    id: string;
     title?: string;
     category: CategoryType;
     details?: string;
