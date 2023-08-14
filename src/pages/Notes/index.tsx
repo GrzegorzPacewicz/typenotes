40b132b4e248c6128d4cc40b437726a7f50308a8
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-08-14 18:49:18 +0200

    UseMutation hook insted of deleteNote function

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { auth, db } from "../../config/firebase";
 import NoteCard from "../../components/NoteCard";
 import Masonry from "@mui/lab/Masonry";
 import { Note } from '../../types';
 import { Container, Typography } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const Notes: React.FC = () => {
     const notesCollectionRef = collection(db, "Notes");
+    const queryClient = useQueryClient();
 
     const {data, status, error} = useQuery({
         queryKey: ["notesData"],
@@ -32,8 +33,17 @@ const Notes: React.FC = () => {
         },
     });
 
-    const deleteNote = () => {
-    };
+    const deleteNoteMutation = useMutation(
+        async (id: string) => {
+            const noteDoc = doc(db, "Notes", id);
+            await deleteDoc(noteDoc);
+        },
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(["notesData"]); // Invalidate the 'notesData' query when deletion is successful
+            },
+        }
+    );
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -47,7 +57,7 @@ const Notes: React.FC = () => {
                 <Masonry spacing={3} columns={{xs: 1, md: 2, lg: 3}}>
                     {data?.map((note) => (
                         <div key={note.id}>
-                            <NoteCard noteProp={note} handleDelete={deleteNote}/>
+                            <NoteCard noteProp={note} handleDelete={() => deleteNoteMutation.mutate(note.id)}/>
                         </div>
                     ))}
                 </Masonry>
